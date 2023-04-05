172b99de4eb1791c347a6e43b2f8b599ef74f339
wPigsy <18710842851.wl@gmail.com>
2023-04-05 11:19:16 +0800

    进度条提交

--- mydialog/src/main/java/com/ruan/mydialog/MainActivity.java
+++ mydialog/src/main/java/com/ruan/mydialog/MainActivity.java
@@ -2,24 +2,15 @@ package com.ruan.mydialog;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.WorkerThread;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.jetbrains.annotations.NotNull;
+import com.ruan.mydialog.uikit.UikitProgress;
 
-import java.io.IOException;
 import java.util.ArrayList;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
@@ -28,6 +19,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        UikitProgress progress = findViewById(R.id.uikitProgress);
+        progress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progress.setProgress(80);
+            }
+        });
 
 
         RecyclerView viewById1 = findViewById(R.id.my_rv);
@@ -43,44 +42,6 @@ public class MainActivity extends Activity {
         viewById1.setAdapter(new DemoAdapter(objects));
 
 
-        View viewById = findViewById(R.id.mtv);
-        viewById.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Interceptor inter = new Interceptor() {
-                    @Override
-                    public Response intercept(Chain chain) throws IOException {
-                        Log.e(TAG, "intercept: dddd");
-
-
-                        String token = getToken();
-
-                        Log.e(TAG, "intercept: dddd222");
-                        return chain.proceed(chain.request());
-                    }
-                };
-                OkHttpClient client = new OkHttpClient.Builder()
-                        .addInterceptor(inter)
-                        .build();
-
-                Request getRequest = new Request.Builder()
-                        .url("https://www.wanandroid.com/article/list/0/json")
-                        .build();
-
-
-                client.newCall(getRequest).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    @Override
-                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                    }
-                });
-
-            }
-        });
     }
 
     @WorkerThread
